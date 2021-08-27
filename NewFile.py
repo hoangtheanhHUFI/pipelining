324e0941623be5d5f8c196f9a8531c86422a9ea2
Cường Thuận <vuongthuan0609@gmail.com>
2021-08-27 19:22:33 +0700

    update code

--- NewFile.py
+++ NewFile.py
@@ -1,11 +1,12 @@
 ﻿from tkinter import *
+from tkinter.ttk import *
 import tkinter
 
 window = Tk()
 window.title("Pipelining ")
 window.geometry("1920x1080")
 # thêm label 
-lbl = Label(window , text= 'Phần mềm mới chế ', fg="green" , font=("Arial",100))
+lbl =tkinter.Label(window , text= 'Phần mềm mới chế ', fg="green" , font=("Arial",100))
 lbl.grid(column = 0 , row = 0 )
 # thêm textbox
 txt = Entry(window, width = 20)
@@ -18,7 +19,14 @@ def handlebutton():
 btnHello = Button (window,text= "say hello", command=handlebutton)
 btnHello.grid(column= 1,row= 1)
 # thêm combobox
-combo = combobox(window)
-
+combo = Combobox(window)
+combo['values'] = ("Cường Thuận", "Thế Anh ", "Tâm Đoàn", " Phước Nhân")
+combo.grid(column=0 , row=2)
+def handlebutton1():
+    lbl.configure(text= 'chào, ' + combo.get() + ' đẹp trai')
+    return
+# thêm button
+btnHello1 = Button (window,text= "Chọn ", command=handlebutton1)
+btnHello1.grid(column= 1,row= 2)
 
 window.mainloop()
