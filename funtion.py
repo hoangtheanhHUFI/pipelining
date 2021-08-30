259ad895fbee6fcb69d4fc117e6b82fce0258225
The Anh <2001202008@hufi.edu.vn>
2021-08-30 21:23:16 +0700

    Update

--- funtion.py
+++ funtion.py
@@ -1,13 +1,13 @@
-class math:
-  def addA():
-    a=int(input())
-  def addB():
-    b=int(input())
-  def showC(a,b):
-    c=a+b
-    return c
-
-  def concho():
-      concho=('hoang the anh') 
-      return concho
-  
+from math import pow
+#n=30
+time_1_lenh=22*pow(10,-9)
+time_1_congdoan=5*pow(10,-9)
+def step_by_step(n):
+    Step=n*time_1_lenh
+    return Step
+def pipelining(n):
+    pipe=5*time_1_congdoan+(n-1)*time_1_congdoan
+    return pipe
+def tile(Step,pipe):
+    tile=pipe/Step
+    return tile
